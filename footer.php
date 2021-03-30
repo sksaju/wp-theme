fbb1acbe03b459318673d9b77e95db8fa9a198b2
Sk Saju <sksaaju@gmail.com>
2021-03-30 22:18:29 +0600

    add header, footer

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Footer template
+ * 
+ * @package wp-theme
+ */
+?>
+
+<footer>Footer</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Header template
+ * 
+ * @package wp-theme
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<?php wp_body_open(); ?>
+
+<header>Header</header>
